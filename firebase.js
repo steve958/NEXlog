584937e8ec13de631516cd0d7aca61294de8b7b1
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2023-03-07 12:40:45 +0100

    firebaseConfig revealed

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_APIKEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECTID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-    appId: process.env.NEXT_PUBLIC_APPID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
+    apiKey: "AIzaSyAgK--XfId4h8PazCSt-ZGutj8JAdWvCb8",
+    authDomain: "nexlog-7b69e.firebaseapp.com",
+    projectId: "nexlog-7b69e",
+    storageBucket: "nexlog-7b69e.appspot.com",
+    messagingSenderId: "82411398467",
+    appId: "1:82411398467:web:019c492fb7c0466fc7fbf6",
+    measurementId: "G-NTSE7SBSD1"
 }
 
 const app = initializeApp(firebaseConfig)
